83fdf7fb0f16f893f619dbb735470f79ef88df9f
oe <yazidkandi4@gmail.com>
2025-02-27 18:38:31 +0100

    webpack cnfig setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',  // Can be 'production' for final build
+    entry: './src/index.js',  // Main JS file
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,  // Clears /dist before each build
     },
-    mode: 'development',
+    devtool: 'inline-source-map',  // Helps with debugging
+    devServer: {
+        static: './dist',  // Serve files from /dist
+        open: true,  // Auto-open browser
+        hot: true,  // Hot module replacement
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',  // Use this HTML template
+            filename: 'index.html',
+        }),
+    ],
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'], // Allows importing CSS files
+            },
+            {
+                test: /\.(png|jpg|jpeg|svg|gif)$/i,
+                type: 'asset/resource', // Allows importing images
             },
         ],
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-        hot: true,
     },
 };
