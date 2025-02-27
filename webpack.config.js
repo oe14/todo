ecc83fda417c3c7cd5cc326d545b6bc7ab9877ab
oe <yazidkandi4@gmail.com>
2025-02-27 18:25:27 +0100

    setup npm & webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+        hot: true,
+    },
+};
