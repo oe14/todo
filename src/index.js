ddb1720d406fc8988f831f585423c61966c0a54f
oe <yazidkandi4@gmail.com>
2025-03-07 17:53:43 +0100

    add cross thru line funcitonality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import './style.css';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    checkboxes.forEach(checkbox => {
+        checkbox.addEventListener("change", function () {
+            const label = this.closest("label");
+            if (this.checked) {
+                label.style.textDecoration = "line-through";
+                label.style.color = "black";
+            } else {
+                label.style.textDecoration = "none";
+                label.style.color = "black";
+            }
+        });
+    });
+});
