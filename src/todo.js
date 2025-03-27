7467b7b5bea57a50da3aedc6d77d8d292fa45cda
oe14 <yazidkandi4@gmail.com>
2025-03-28 00:39:42 +0100

    addtask && addtodo logic

--- src/todo.js
+++ src/todo.js
@@ -1,37 +1,50 @@
-const maketodo(name, description, dueDate, priority){
-return{ 
-name, 
-description, 
-dueDate, 
-priority, 
-completed: false,
+class task{ 
+
+constructor(name, description = "", priority = "normal", completed = "false"){
+    this.name = name; 
+    this.description = description; 
+    this.priority = priority; 
+    this.completed = completed; 
 
 }
+
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+  
+ edit(name, description, priority) {
+    if (name) this.name = name;
+    if (description) this.description = description;
+    if (priority) this.priority = priority;
+  }
 }
 
 
-const createProject = (name) => {
-    return {
-        name, 
-        todos: [],  
-        
-        addTodo(todo) {
-            this.todos.push(todo);  t
-        }, 
-        
-        removeTodo(index) {
-            this.todos.splice(index, 1);  
-        }
-    };
-};
+class Todo {
+  constructor(type) {
+    this.type = type; // personal/work
+    this.tasks = [];
+  }
+
+ addTask(task) {
+    this.tasks.push(task);
+  }
+
+  removeTask(name) {
+    this.tasks = this.tasks.filter(task => task.name !== name);
+  }
+    getTasks() {
+    return this.tasks; // Returns the array of tasks
+}
+
+
+const holiday = new Todo("personal"); 
+const task1 = new Task("Buy gifts", "Get gifts for family", "high");
+const task2 = new Task("Pack bags", "Prepare luggage", "medium");
 
 
 
+holiday.addTask(task1);
+holiday.addTask(task2);
 
-
-
-// storing projects in a array
-const projects = [], 
-const defaultProject = createProject("default");
-projects.push(defaultProject);
-
+console.log(holiday.getTasks()); 
