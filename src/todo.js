b4255e8ab109f94541da16a111ceade722c2e97b
oe14 <yazidkandi4@gmail.com>
2025-03-31 05:03:59 +0100

    clear index

--- src/todo.js
+++ src/todo.js
@@ -1,24 +1,21 @@
-class task{ 
-
-constructor(name, description = "", priority = "normal", completed = "false"){
+class Task { 
+  constructor(name, description = "", priority = "normal", completed = false) {
     this.name = name; 
     this.description = description; 
     this.priority = priority; 
     this.completed = completed; 
-
-}
+  }
 
   toggleComplete() {
     this.completed = !this.completed;
   }
   
- edit(name, description, priority) {
+  edit(name, description, priority) {
     if (name) this.name = name;
     if (description) this.description = description;
     if (priority) this.priority = priority;
   }
 }
-
 
 class Todo {
   constructor(type) {
@@ -26,25 +23,25 @@ class Todo {
     this.tasks = [];
   }
 
- addTask(task) {
+  addTask(task) {
     this.tasks.push(task);
   }
 
   removeTask(name) {
     this.tasks = this.tasks.filter(task => task.name !== name);
   }
-    getTasks() {
-    return this.tasks; // Returns the array of tasks
-}
 
+  getTasks() {
+    return this.tasks; 
+  }
+}
 
 const holiday = new Todo("personal"); 
 const task1 = new Task("Buy gifts", "Get gifts for family", "high");
 const task2 = new Task("Pack bags", "Prepare luggage", "medium");
 
-
-
 holiday.addTask(task1);
 holiday.addTask(task2);
 
 console.log(holiday.getTasks()); 
+
